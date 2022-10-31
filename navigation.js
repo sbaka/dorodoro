8bae14537514ba5cd074303218d8b2fc50c4c19e
sbaka <abdelatif4614@gmail.com>
2022-10-31 12:17:57 +0100

    final adjustments to the design

---
+++ navigation.js
@@ -0,0 +1,11 @@
+function goSignIn() {
+    location.href = "./signIn.html";
+};
+
+function goSignUp() {
+    location.href = "./signUp.html";
+};
+
+function goHome() {
+    location.href = "./index.html"
+}
