c30f57421d00eae8daffae86d6d7411c26206a7a
Latif_Kbr <abdelatif4614@gmail.com>
2022-11-28 00:31:57 +0100

    added timer section

---
+++ timer.js
@@ -0,0 +1,11 @@
+var countdownNumberEl = document.getElementById('countdown-number');
+var countdown = 40;
+countdownNumberEl.textContent = countdown;
+
+// setInterval(function () {
+//     countdown = --countdown <= 0 ? 10 : countdown;
+
+//     countdownNumberEl.textContent = countdown;
+// }, 1000);
+
+// var circle = document.getElementById("svg").style.animation = `countdown ${countdown}s linear infinite forwards`
